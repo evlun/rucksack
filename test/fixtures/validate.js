1fd4c22a6ea4048639238aaa420540e118d63d2e
Erik Lundin <mjor.himself@gmail.com>
2012-03-19 03:56:51 +0100

    Lint `test/fixtures/validate.js`

--- test/fixtures/validate.js
+++ test/fixtures/validate.js
@@ -5,14 +5,14 @@ function inspect(buffer) {
   return Array.prototype.map.call(buffer, function(byte) {
     return (byte < 16 ? '0' + byte.toString(16) : byte.toString(16));
   }).join(' ');
-};
+}
 
 function pack(input, expected) {
   return function() {
     var result = square.pack(input);
     assert.deepEqual(inspect(result), expected);
   };
-};
+}
 
 pack.batch = function(arr) {
   return function() {
