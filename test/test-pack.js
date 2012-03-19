842a12c2e3bd8aa433649aed88409b6af5e16810
Erik Lundin <mjor.himself@gmail.com>
2012-03-19 19:55:40 +0100

    Add further object packing tests

--- test/test-pack.js
+++ test/test-pack.js
@@ -106,6 +106,12 @@ module.exports = {
     '{ b: 2, c: 3 }': v({ b: 2, c: 3 }, 'c2 01 62 02 01 63 03'),
     '{ \'√3.\': 4 }': v({ '√3.': 4 },   'c1 05 e2 88 9a 33 2e 04'),
 
+    '{ d: { e: { f: 5, g: 6 } } }':
+      v({ d: { e: { f: 5, g: 6 } } }, 'c1 01 64 c1 01 65 c2 01 66 05 01 67 06'),
+
+    '{ h: \'abc\', i: \'åäö\' }': v({ h: 'abc', i: 'åäö' },
+      'c2 01 68 e3 61 62 63 01 69 e6 c3 a5 c3 a4 c3 b6'),
+
     'ignore inherited/prototype properties':
       v((function() {
         function X() { this.y = 10; }
