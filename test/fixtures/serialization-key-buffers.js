f03a5fc2e64d263e84e1d0fc240fb5bcc0b97c38
Erik Lundin <mjor.himself@gmail.com>
2012-03-22 16:53:02 +0100

    Add buffer packing tests

---
+++ test/fixtures/serialization-key-buffers.js
@@ -0,0 +1,25 @@
+var buf = new Buffer('123'),
+    arr = [1, 2, buf, 4],
+    shared = new Buffer('0123456789');
+
+module.exports = {
+  'new Buffer(\'123\')':            [buf, 'ab 00 03 31 32 33'],
+  '[1, 2, new Buffer(\'123\'), 4]': [arr, 'd4 01 02 ab 00 03 04 31 32 33'],
+
+  '[new Buffer(\'1\'), new Buffer(\'2\')]': [
+    [new Buffer('1'), new Buffer('2')], 'd2 ab 00 01 ab 01 01 31 32'
+  ],
+
+  'a buffer and a slice of it': [
+    [buf, buf.slice(1, 3)], 'd2 ab 00 03 ab 01 02 31 32 33'
+  ],
+
+  'two distant buffer views': [
+    [shared.slice(8, 10), shared.slice(0, 2)], 'd2 ab 02 02 ab 00 02 30 31'
+  ],
+
+  'two overlapping buffer views': [
+    [shared.slice(2, 6), shared.slice(4, 8)],
+    'd2 ab 00 04 ab 02 04 32 33 34 35 36 37'
+  ]
+};
