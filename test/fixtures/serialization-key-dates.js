25563ce578cf73529e108b57726869a299d7e1f9
Erik Lundin <mjor.himself@gmail.com>
2012-03-26 11:25:12 +0200

    Significant rewrite of both packing and unpacking components
    This should have been split up into separate commits, but the changes ended up
    too intertwined.
    
      1. Removed support for Buffers
      2. Changed the Date type's token to `0xab`, previously used by the Buffer type
      3. Rewrote `unpack.js` completely, now passes all tests
      4. Cleaned `pack.js` up a bit

--- test/fixtures/serialization-key-dates.js
+++ test/fixtures/serialization-key-dates.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'new Date(0)':         [new Date(0),         'ad 00'],
-  'new Date(123456789)': [new Date(123456789), 'ad 95 9a ef 3a']
+  'new Date(0)':         [new Date(0),         'ab 00'],
+  'new Date(123456789)': [new Date(123456789), 'ab 95 9a ef 3a']
 };
